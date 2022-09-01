9eebf893aeeaf7022602dc5f41b834b3ac76e4f4
thewickest <graubuntu@gmail.com>
2022-09-01 18:10:02 +0200

    Minor Changes

--- src/main/java/mca/prices_rest_api/dao/PricesDao.java
+++ src/main/java/mca/prices_rest_api/dao/PricesDao.java
@@ -5,13 +5,21 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import mca.prices_rest_api.entity.Price;
 
-@Repository
 public interface PricesDao extends JpaRepository<Price, Long> {
 
+	/**
+	 * 
+	 * Extrae una lista de precios filtrados por brandId y productId 
+	 * con la fecha de aplicaion contenida entre Fecha de Inicio y Fecha Fin
+	 * 
+	 * @param brandId
+	 * @param productId
+	 * @param applyDate
+	 * @return
+	 */	
 	@Query(value = "SELECT p FROM Price p WHERE p.brandId = ?1 AND p.productId = ?2 AND p.startDate <= ?3 AND p.endDate > ?3 ORDER BY p.priority DESC")
 	public List<Price> findAllByBrandProductAndApplyDate(int brandId, int productId, Date applyDate);
 
--- src/main/java/mca/prices_rest_api/services/impl/PricesServiceImpl.java
+++ src/main/java/mca/prices_rest_api/services/impl/PricesServiceImpl.java
@@ -1,6 +1,7 @@
 package mca.prices_rest_api.services.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +21,21 @@ public class PricesServiceImpl implements PricesService {
 	@Override
 	public JSONObject getPrices(int brandId, int productId, Date applyDate) {
 
-		Price p = pricesDao.findAllByBrandProductAndApplyDate(brandId, productId, applyDate).get(0);
-
+		List<Price> prices = pricesDao.findAllByBrandProductAndApplyDate(brandId, productId, applyDate);
 		JSONObject jsonPrice = new JSONObject();
 
-		jsonPrice.put("product_id", p.getProductId());
-		jsonPrice.put("brand_id", p.getBrandId());
-		jsonPrice.put("prices_list", p.getPriceId());
-		jsonPrice.put("start_date", p.getStartDate().toString());
-		jsonPrice.put("end_date", p.getEndDate().toString());
-		jsonPrice.put("price", p.getPrice() + " " + p.getCurrency());
+		if (prices != null && !prices.isEmpty()) {
+			Price p = pricesDao.findAllByBrandProductAndApplyDate(brandId, productId, applyDate).get(0);
+
+			jsonPrice.put("product_id", p.getProductId());
+			jsonPrice.put("brand_id", p.getBrandId());
+			jsonPrice.put("prices_list", p.getPriceId());
+			jsonPrice.put("start_date", p.getStartDate().toString());
+			jsonPrice.put("end_date", p.getEndDate().toString());
+			jsonPrice.put("price", p.getPrice() + " " + p.getCurrency());
+		} else {
+			jsonPrice.put("error", "No prices found");
+		}
 
 		return jsonPrice;
 	}
